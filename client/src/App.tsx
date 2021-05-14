fc0c003c2edb59d9560d0b96367777dd4be5b8fe
Kent Claudio <rikysmile33@gmail.com>
2021-05-13 23:01:58 -0700

    Created NavBar component and updated directory
    Added components and styles folder and placed respective files to these
    folders
    
    Installed react-router-dom for NavBar component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,23 +1,27 @@
 import React from "react";
-import "./App.css";
+import "./styles/App.css";
+import NavBar from "./components/NavBar";
+import { BrowserRouter as Router } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="headerContainer">
-        <p>This is header</p>
-      </header>
-      <main className="mainContainer">
-        <article className="articleContainer">
-          <p>This is article 1</p>
-        </article>
-        <article className="articleContainer">
-          <p>This is article 2</p>
-        </article>
-      </main>
-      <footer className="footerContainer">
-        <p>This is footer</p>
-      </footer>
+      <Router>
+        <header className="headerContainer">
+          <NavBar />
+        </header>
+        <main className="mainContainer">
+          <article className="articleContainer">
+            <p>This is article 1</p>
+          </article>
+          <article className="articleContainer">
+            <p>This is article 2</p>
+          </article>
+        </main>
+        <footer className="footerContainer">
+          <p>Website by Kent Claudio</p>
+        </footer>
+      </Router>
     </div>
   );
 }
---
+++ client/src/components/NavBar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <nav className="navBar">
+      <p>This is nav bar</p>
+    </nav>
+  );
+};
+
+export default NavBar;
