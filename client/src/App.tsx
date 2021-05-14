eb9618abc825734573fa1e434fdf9336d576e6bb
Kent Claudio <rikysmile33@gmail.com>
2021-05-13 21:19:04 -0700

    Updated website styling
    Added text file that holds format example of icons object for
    public/manifest.json

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="headerContainer">
+        <p>This is header</p>
       </header>
+      <main className="mainContainer">
+        <article className="articleContainer">
+          <p>This is article 1</p>
+        </article>
+        <article className="articleContainer">
+          <p>This is article 2</p>
+        </article>
+      </main>
+      <footer className="footerContainer">
+        <p>This is footer</p>
+      </footer>
     </div>
   );
 }
