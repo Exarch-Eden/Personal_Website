5986cfd286e5050f502efc28a07cc31430fb29cb
Kent Claudio <rikysmile33@gmail.com>
2021-05-15 22:11:46 -0700

    Implemented Doughnut chart to About page

--- client/src/screens/About.tsx
+++ client/src/screens/About.tsx
@@ -1,9 +1,27 @@
 import React, { useEffect, useState } from "react";
+import { Doughnut } from "react-chartjs-2";
 
 // css
 import "../styles/Universal.css";
+import "../styles/About.css"
 
 const localHostApiUrl = "http://localhost:5000/api";
+
+const data = {
+  labels: ["Red", "Blue", "Yellow"],
+  datasets: [
+    {
+      label: "First dataset",
+      data: [300, 50, 100],
+      backgroundColor: ["#71c6d0", "#000000", "#282c34"],
+      hoverOffset: 4,
+    },
+  ],
+};
+
+const options = {
+  maintainAspectRatio: false,
+};
 
 const About = () => {
   const [languageData, setLanguageData] = useState({});
@@ -37,6 +55,9 @@ const About = () => {
       <div>
         <p>Languages used for this project:</p>
         <p>{JSON.stringify(languageData)}</p>
+        <div className="doughnutChartContainer">
+          <Doughnut data={data} type="doughnut" options={options} />
+        </div>
       </div>
     </article>
   );
