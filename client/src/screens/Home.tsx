8f504dffe832e1b1849383526debbe380ebffc8d
Kent Claudio <rikysmile33@gmail.com>
2021-05-16 04:24:58 -0700

    Added ComplexCard component to Home page

--- client/src/screens/Home.tsx
+++ client/src/screens/Home.tsx
@@ -1,7 +1,9 @@
 import React from "react";
+import ComplexCard from "../components/ComplexCard";
 
 // css
 import "../styles/Universal.css";
+import "../styles/Home.css";
 
 const Home = () => {
   return (
@@ -11,6 +13,9 @@ const Home = () => {
       </div>
       <div>
         <p>Main content</p>
+        <div className="cardLayoutContainer paddedContainer">
+          <ComplexCard />
+        </div>
       </div>
     </article>
   );
