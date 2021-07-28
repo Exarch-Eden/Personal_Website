f561ad6c780657cfb0215fba7b00595219ffa447
Kent Claudio <rikysmile33@gmail.com>
2021-07-27 20:00:08 -0700

    Updated HomeCanvas component
    Also updated HomeCanvas CSS

--- client/src/components/HomeCanvas.tsx
+++ client/src/components/HomeCanvas.tsx
@@ -1,7 +1,8 @@
 import { Canvas, Vector3 } from "@react-three/fiber";
-import React, { Suspense } from "react";
+import React, { Suspense, useRef } from "react";
 import Cloud from "./Cloud";
 import Mountains from "./Mountains";
+import * as THREE from "three";
 
 // css
 import "../styles/HomeCanvas.css";
@@ -11,8 +12,7 @@ import "../styles/HomeCanvas.css";
 // example: info regarding vector, type of object (cloud or mountain),
 //  and necessary parameters for said plane object
 
-
-const CameraPosition: Vector3 = [0, 0, 10];
+const CameraPosition: Vector3 = [0, 0, 30];
 const CloudSmallLeft1Position: Vector3 = [-14, 1, 0.5];
 const CloudMediumLeft1Position: Vector3 = [-20, -1, 2];
 const Mountains1Position: Vector3 = [0, 0, 0];
@@ -21,12 +21,12 @@ const MountainsBackPosition: Vector3 = [0, 0, 0];
 
 const CloudMediumLeft1 = {
   position: CloudMediumLeft1Position,
-  scale: 0.5
+  scale: 0.5,
 };
 
 const CloudSmallLeft1 = {
   position: CloudSmallLeft1Position,
-  scale: 0.5
+  scale: 0.5,
 };
 
 const Mountains1 = {
@@ -39,12 +39,44 @@ const MountainsFront = {
 };
 const MountainsBack = {
   position: MountainsBackPosition,
-  scale: 1.1
+  scale: 1.1,
 };
 
 const CanvasCamera = {
   position: CameraPosition,
-}
+};
+
+const GroupTop = () => {
+  return (
+    <group>
+      <Cloud
+        size="medium"
+        type="long"
+        rest={CloudMediumLeft1}
+        initialPos={-17}
+      />
+      <Cloud size="small" type="long" rest={CloudSmallLeft1} initialPos={-14} />
+      {/* <Mountains type="all" rest={Mountains1} /> */}
+      <Mountains type="front" rest={MountainsFront} />
+      <Mountains type="back" rest={MountainsBack} />
+    </group>
+  );
+};
+
+const GroupMid = () => {
+  const backgroundPosition: Vector3 = [0, -52.975, 0]
+
+  const mesh = useRef<THREE.Mesh>(null!);
+
+  return (
+    <group>
+      <mesh ref={mesh} position={backgroundPosition}>
+        <planeGeometry attach="geometry" args={[100, 100]} />
+        <meshStandardMaterial color="black" side={THREE.FrontSide} />
+      </mesh>
+    </group>
+  );
+};
 
 const HomeCanvas = () => {
   return (
@@ -52,21 +84,8 @@ const HomeCanvas = () => {
       {/* <ambientLight /> */}
       {/* <pointLight position={pointLightVector} /> */}
       <Suspense fallback={null}>
-        <Cloud
-          size="medium"
-          type="long"
-          rest={CloudMediumLeft1}
-          initialPos={-17}
-        />
-        <Cloud
-          size="small"
-          type="long"
-          rest={CloudSmallLeft1}
-          initialPos={-14}
-        />
-        {/* <Mountains type="all" rest={Mountains1} /> */}
-        <Mountains type="front" rest={MountainsFront} />
-        <Mountains type="back" rest={MountainsBack} />
+        <GroupTop />
+        <GroupMid />
       </Suspense>
     </Canvas>
   );
