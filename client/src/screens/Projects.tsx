40f2471dd53ed25e38e0578b5eaff76cc50846b9
Kent Claudio <rikysmile33@gmail.com>
2021-08-04 14:05:19 -0700

    Created Projects screen

---
+++ client/src/screens/Projects.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+// css
+import "../styles/Universal.css"
+
+const Projects = () => {
+  return (
+    <article className="projectsContainer articleContainer">
+      <div className="titleContainer"><p>Projects Page</p></div>
+
+    </article>
+  )
+}
+
+export default Projects
