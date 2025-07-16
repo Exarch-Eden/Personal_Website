d8f5cf19bca67972cea721dee532fbb2c8341346
Exarch-Eden <rikysmile33@gmail.com>
2025-07-15 17:57:59 -0700

    Auto formatted

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import { Canvas } from "@react-three/fiber";
 import { Suspense } from "react";
@@ -8,18 +8,29 @@ import { OrbitControls } from "@react-three/drei";
 const Home = () => {
     return (
         <div className="flex min-h-screen border border-blue-500 items-center justify-center">
-            <Canvas style={{ height: window.innerHeight, width: window.innerWidth }} camera={{ position: [0,0,5], fov: 75 }}>
+            <Canvas
+                style={{ height: window.innerHeight, width: window.innerWidth }}
+                camera={{ position: [0, 0, 5], fov: 75 }}
+            >
                 <Suspense fallback={null}>
                     <ambientLight intensity={0.5} />
-                    <directionalLight position={[10,10,5]} intensity={1} castShadow />
-                    <spotLight position={[10,10,10]} angle={0.15} penumbra={1} />
-                    <pointLight position={[-10,-10,-10]} intensity={0.5} />
+                    <directionalLight
+                        position={[10, 10, 5]}
+                        intensity={1}
+                        castShadow
+                    />
+                    <spotLight
+                        position={[10, 10, 10]}
+                        angle={0.15}
+                        penumbra={1}
+                    />
+                    <pointLight position={[-10, -10, -10]} intensity={0.5} />
                     <TownModel />
                     <OrbitControls />
                 </Suspense>
             </Canvas>
         </div>
-    )
+    );
 };
 
 export default Home;
