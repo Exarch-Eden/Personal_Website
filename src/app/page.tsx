7020e5badace74c98a80fea91eb8654fb6fbd249
Exarch-Eden <rikysmile33@gmail.com>
2025-07-15 18:18:55 -0700

    Updated global page
    Separated Canvas and child components into its own

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,30 +5,35 @@ import { Suspense } from "react";
 import { TownModel } from "./library";
 import { OrbitControls } from "@react-three/drei";
 
+const HeroSection = () => {
+    return (
+        <Canvas
+            style={{ height: window.innerHeight, width: window.innerWidth }}
+            orthographic
+            camera={{ position: [10, 5, 10], zoom: 50 }}
+            // orthographic camera is preferred for model showcase
+            // camera={{ position: [100, 50, 100], fov: 50 }}
+        >
+            <Suspense fallback={null}>
+                <ambientLight intensity={0.5} />
+                <directionalLight
+                    position={[10, 10, 5]}
+                    intensity={1}
+                    castShadow
+                />
+                <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
+                <pointLight position={[-10, -10, -10]} intensity={0.5} />
+                <TownModel />
+                <OrbitControls />
+            </Suspense>
+        </Canvas>
+    );
+};
+
 const Home = () => {
     return (
         <div className="flex min-h-screen border border-blue-500 items-center justify-center">
-            <Canvas
-                style={{ height: window.innerHeight, width: window.innerWidth }}
-                camera={{ position: [0, 0, 5], fov: 75 }}
-            >
-                <Suspense fallback={null}>
-                    <ambientLight intensity={0.5} />
-                    <directionalLight
-                        position={[10, 10, 5]}
-                        intensity={1}
-                        castShadow
-                    />
-                    <spotLight
-                        position={[10, 10, 10]}
-                        angle={0.15}
-                        penumbra={1}
-                    />
-                    <pointLight position={[-10, -10, -10]} intensity={0.5} />
-                    <TownModel />
-                    <OrbitControls />
-                </Suspense>
-            </Canvas>
+            <HeroSection />
         </div>
     );
 };
